4089c9f498dbf1e24e57bcab26b88d38970a3c19
ahmed firoum <ahmedfiroumk@gmail.com>
2025-06-13 15:33:30 +0200

    chore/cleaning done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
-
+import "./global.css";
 /* ************************************************************************* */
 
 // Import the main app component
