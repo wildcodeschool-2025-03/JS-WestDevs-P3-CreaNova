83a70e7e66b3caba20af9df2548b01b438697c8a
TRISTAN ZUBIARRAIN <TRISTANZUBIARRAIN15@gmail.com>
2025-06-18 17:15:42 +0200

    work in progress

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -2,9 +2,39 @@ import "./HomePage.css";
 
 function HomePage() {
   return (
-    <>
-      <h1>JE SUIS SUR LA PAGE HOME</h1>
-    </>
+    <main className="main-home">
+      <section className="title">
+        <h1>CreaNova</h1>
+        <p>L’innovation créative à portée de main</p>
+      </section>
+      <section className="art-preview">
+        <p>
+          L’art est une expression unique de créativité et d’émotion. Chaque
+          œuvre raconte une histoire et invite à découvrir de nouveaux horizons.
+          Plongez dans cet univers passionnant.
+        </p>
+        <img src="Oeuvre-homePage.webp" alt="Le vent souffle" />
+      </section>
+      <p className="site-intro">
+        CreaNova est une galerie d’art en ligne qui réunit photos, peintures et
+        dessins d’artistes passionnés. Chaque créateur peut ajouter ses œuvres,
+        enrichissant ainsi la collection avec des pièces uniques. Que vous soyez
+        amateur ou collectionneur, trouvez des œuvres originales à acheter pour
+        personnaliser votre intérieur. CreaNova, une galerie vivante où l’art
+        s’invite chez vous en toute simplicité.
+      </p>
+      <section className="artiste">
+        <h2>Artistes</h2>
+        <p>
+          Découvrez les talents qui donnent vie à notre galerie. Qu'il s'agisse
+          de peinture ou de photographie, chaque artiste explore des univers
+          uniques. Leurs œuvres sont des invitations à la réflexion et à
+          l'émotion. Laissez-vous transporter par leurs visions singulières.
+        </p>
+        <img src="artiste-homepage.webp" alt="Bertrand Rogon" />
+        <figcaption>Accéder aux aristes</figcaption>
+      </section>
+    </main>
   );
 }
 
