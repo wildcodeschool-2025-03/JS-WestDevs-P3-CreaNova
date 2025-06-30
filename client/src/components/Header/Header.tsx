abd008e6cf5fbd42465e05885a5a03a98033425d
ahmed firoum <ahmedfiroumk@gmail.com>
2025-06-30 23:21:36 +0200

    fixbug center nav wip

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -13,143 +13,131 @@ const Header = () => {
     setIsOpen(false);
   };
   return (
-    <>
-      <header>
-        <section className="menu-et-logo">
-          <Menu
-            isOpen={isOpen}
-            onOpen={handleOpenMenu}
-            onClose={handleCloseMenu}
-          >
-            <Link
-              id="artist"
-              className="menu-item"
-              to="/artist"
-              onClick={handleCloseMenu}
-            >
-              Artistes
+    <header>
+      <Menu isOpen={isOpen} onOpen={handleOpenMenu} onClose={handleCloseMenu}>
+        <Link
+          id="artist"
+          className="menu-item"
+          to="/artist"
+          onClick={handleCloseMenu}
+        >
+          Artistes
+        </Link>
+
+        <Link
+          id="peintures"
+          className="menu-item"
+          to="/peintures"
+          onClick={handleCloseMenu}
+        >
+          Peintures
+        </Link>
+
+        <Link
+          id="photographies"
+          className="menu-item"
+          to="/gallery_page_photo"
+          onClick={handleCloseMenu}
+        >
+          Photographies
+        </Link>
+
+        <Link
+          id="dessins"
+          className="menu-item"
+          to="/dessins"
+          onClick={handleCloseMenu}
+        >
+          Dessins
+        </Link>
+
+        <Link
+          id="newsletter"
+          className="menu-item"
+          to="/newsletter"
+          onClick={handleCloseMenu}
+        >
+          Newsletters
+        </Link>
+
+        <Link
+          id="a propos"
+          className="menu-item"
+          to="/a propos"
+          onClick={handleCloseMenu}
+        >
+          A propos
+        </Link>
+      </Menu>
+      <section className="left_nav">
+        <Link to="/">
+          <img className="logo" src="/img/black_logo.png" alt="logo-black" />
+        </Link>
+        <nav className="nav_categories">
+          <button type="button">
+            <Link to="/artist">
+              <span>Artistes</span>
             </Link>
+          </button>
 
-            <Link
-              id="peintures"
-              className="menu-item"
-              to="/peintures"
-              onClick={handleCloseMenu}
-            >
-              Peintures
+          <button type="button">
+            <Link to="/peintures">
+              <span>Peintures</span>
             </Link>
-
-            <Link
-              id="photographies"
-              className="menu-item"
-              to="/gallery_page_photo"
-              onClick={handleCloseMenu}
-            >
-              Photographies
+          </button>
+          <button type="button">
+            <Link to="/gallery_page_photo">
+              <span>Photographies</span>
             </Link>
+          </button>
 
-            <Link
-              id="dessins"
-              className="menu-item"
-              to="/dessins"
-              onClick={handleCloseMenu}
-            >
-              Dessins
+          <button type="button">
+            <Link to="/dessins">
+              <span>Dessins</span>
             </Link>
+          </button>
 
-            <Link
-              id="newsletter"
-              className="menu-item"
-              to="/newsletter"
-              onClick={handleCloseMenu}
-            >
-              Newsletters
+          <button type="button">
+            <Link to="/newsletter">
+              <span>Newsletter</span>
             </Link>
-
-            <Link
-              id="a propos"
-              className="menu-item"
-              to="/a propos"
-              onClick={handleCloseMenu}
-            >
-              A propos
+          </button>
+          <button type="button">
+            <Link to="/a propos">
+              <span>A propos</span>
             </Link>
-          </Menu>
-          <section className="left_nav">
-            <Link to="/">
-              <img
-                className="logo"
-                src="/img/black_logo.png"
-                alt="logo-black"
-              />
+          </button>
+        </nav>
+
+        <nav className="nav_menu">
+          <Link to="/dark_mode">
+            <img src="/img/soleil.png" alt="dark_mode" />
+          </Link>
+          <Link to="/favoris">
+            <img src="/img/amour-du-coeur.png" alt="coeur" />
+          </Link>
+          <Link to="/panier">
+            <img src="/img/panier.png" alt="panier" />
+          </Link>
+
+          <button type="button" popoverTarget="contact_modal">
+            <img src="/img/contact.png" alt="contact" />
+          </button>
+          <dialog popover="auto" id="contact_modal">
+            <Link to="/" id="contact_logo">
+              <img src="/img/contact.png" alt="contact" />
             </Link>
-            <nav className="nav_categories">
-              <button type="button">
-                <Link to="/artist">
-                  <span>Artistes</span>
-                </Link>
-              </button>
-
-              <button type="button">
-                <Link to="/peintures">
-                  <span>Peintures</span>
-                </Link>
-              </button>
-              <button type="button">
-                <Link to="/gallery_page_photo">
-                  <span>Photographies</span>
-                </Link>
-              </button>
-
-              <button type="button">
-                <Link to="/dessins">
-                  <span>Dessins</span>
-                </Link>
-              </button>
-
-              <button type="button">
-                <Link to="/newsletter">
-                  <span>Newsletter</span>
-                </Link>
-              </button>
-              <button type="button">
-                <Link to="/a propos">
-                  <span>A propos</span>
-                </Link>
-              </button>
-            </nav>
-
-            <nav className="nav_menu">
-              <Link to="/dark_mode">
-                <img src="/img/soleil.png" alt="dark_mode" />
-              </Link>
-              <Link to="/favoris">
-                <img src="/img/amour-du-coeur.png" alt="coeur" />
-              </Link>
-              <Link to="/panier">
-                <img src="/img/panier.png" alt="panier" />
-              </Link>
-
-              <button type="button" popoverTarget="contact_modal">
-                <img src="/img/contact.png" alt="contact" />
-              </button>
-              <dialog popover="auto" id="contact_modal">
-                <Link to="/" id="contact_logo">
-                  <img src="/img/contact.png" alt="contact" />
-                </Link>
-                <br />
-                <Link to="/ma collection">Ma collection</Link>
-                <hr />
-                <Link to="/favoris">Favoris</Link>
-                <hr />
-                <Link to="/login">Connexion</Link>
-              </dialog>
-            </nav>
-          </section>
-          <hr className="line" />
-        </section>
-      </header>
-    </>
+            <br />
+            <Link to="/ma collection">Ma collection</Link>
+            <hr />
+            <Link to="/favoris">Favoris</Link>
+            <hr />
+            <Link to="/login">Connexion</Link>
+          </dialog>
+        </nav>
+      </section>
+      <hr className="line" />
+    </header>
   );
 };
 
