afabb38b6ccb91c4e8f6d0a8b9068ff16df3ced3
mohamed <aouis.mohamed@yahoo.fr>
2025-07-01 14:43:00 +0200

    resolve conflict biome

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,35 +1,33 @@
 import "./Footer.css";
 
-
 function Footer() {
   return (
     <>
-    <hr className="footer-line" />
-    <footer className="banner-footer">
-      
-      <ul className="social-icons">
-        <li>
-          <a href="https://www.instagram.com/" target="blank">
-            <img src="/image/instagram.png" alt="logo instagram" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/" target="blank">
-            <img src="/image/facebook.png" alt="logo facebook" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/" target="blank">
-            <img src="/image/linkedin.png" alt="logo linkedin" />
-          </a>
-        </li>
-        <li>
-          <a href="https://x.com/" target="blank">
-            <img src="/image/twitter.png" alt="logo twitter" />
-          </a>
-        </li>
-      </ul>
-    </footer>
+      <hr className="footer-line" />
+      <footer className="banner-footer">
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.instagram.com/" target="blank">
+              <img src="/image/instagram.png" alt="logo instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/" target="blank">
+              <img src="/image/facebook.png" alt="logo facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/" target="blank">
+              <img src="/image/linkedin.png" alt="logo linkedin" />
+            </a>
+          </li>
+          <li>
+            <a href="https://x.com/" target="blank">
+              <img src="/image/twitter.png" alt="logo twitter" />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </>
   );
 }
