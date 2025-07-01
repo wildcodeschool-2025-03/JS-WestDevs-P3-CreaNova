302c2733c9e52cceafe1080199a2b001603aa50e
anne durand <annedurand0709@gmail.com>
2025-07-01 11:46:46 +0200

    css gallery page

--- client/src/pages/GalleryPage/GalleryPage.tsx
+++ client/src/pages/GalleryPage/GalleryPage.tsx
@@ -12,12 +12,20 @@ function GalleryPage() {
   return (
     <main className="gallery_page">
       <h1 className="title"> Gallerie</h1>
+      <div id="search_bar">
+        <button className="search" type="button">
+          Rechercher une oeuvre...
+        </button>
+      </div>
 
       {artwork.map((artwork) => (
         <figure key={artwork.id}>
+          <img className="favorite" src="img/favorite.png" alt="favorite" />
           <img src={artwork.image} alt={artwork.title} />
+
           <figcaption>
             {artwork.firstname} {artwork.lastname}
+            <img src="img/shopping-cart-white-icon.png" alt="panier" />
           </figcaption>
         </figure>
       ))}
