4ec37c8e4e1591820c4cf31c72a738762e6ec9e5
TRISTAN ZUBIARRAIN <TRISTANZUBIARRAIN15@gmail.com>
2025-07-01 11:37:47 +0200

    work in progress

---
+++ client/src/pages/UserFormPage/UserFormPage.tsx
@@ -0,0 +1,36 @@
+function UserFormPage() {
+  return (
+    <main className="user-form-main">
+      <section>
+        <img src="" alt="" />
+        <figcaption>Zubiarrain Tristan</figcaption>
+
+        <form action="{handleSubmit}">
+          <label htmlFor="lastname">Votre nom</label>
+          <input type="text" placeholder="ex: Dupont" name="lastname" />
+          <label htmlFor="firstname">Prénom</label>
+          <input name="firstname" type="text" placeholder="ex: Jean" />
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="ex: jean.dupont@example.com" />
+          <label htmlFor="street">Adresse</label>
+          <input type="text" placeholder="ex: 123 rue de Paris" name="street" />
+          <label htmlFor="city">Ville</label>
+          <input type="text" placeholder="ex: Paris" name="city" />
+          <label htmlFor="postalCode">Code Postal</label>
+          <input type="text" placeholder="ex: 75000" name="zip_code" />
+          <label htmlFor="country">Pays</label>
+          <input type="text" placeholder="ex: France" name="country" />
+          <label htmlFor="image">Image</label>
+          <input type="file" name="image" accept="image/*" />
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            placeholder="ex: Passionné d'art contemporain..."
+            name="description"
+          />
+        </form>
+      </section>
+    </main>
+  );
+}
+export default UserFormPage;
