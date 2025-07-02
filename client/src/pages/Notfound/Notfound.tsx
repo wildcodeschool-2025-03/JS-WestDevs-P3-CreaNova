5c778147c4b2adb64c73e8a6629791e085a42211
mohamed <aouis.mohamed@yahoo.fr>
2025-07-02 11:15:31 +0200

    merge whith dev

--- client/src/pages/Notfound/Notfound.tsx
+++ client/src/pages/Notfound/Notfound.tsx
@@ -1,10 +1,8 @@
 import "./NotFound.css";
-import { Link } from "react-router";
 function NotFound() {
   return (
     <main className="notfound-container">
       <img src="/image/404.png" alt="page404" />
-      <Link to="/">homepage</Link>
     </main>
   );
 }
