309300a81cf56b06671d5894d4e2d2a8ecc7f49d
anne durand <annedurand0709@gmail.com>
2025-07-04 11:19:00 +0200

    resolve conflict

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,12 +2,12 @@ import { createBrowserRouter } from "react-router";
 import App from "./App";
 import ArtistDetailsPage from "./pages/ArtistDetailsPage/ArtistDetailsPage";
 import ArtistPage from "./pages/ArtistPage/ArtistPage";
+import CollectionPage from "./pages/CollectionPage/CollectionPage";
 import GalleryPage from "./pages/GalleryPage/GalleryPage";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import NotFound from "./pages/Notfound/Notfound";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
-import CollectionPage from "./pages/CollectionPage/CollectionPage";
 
 const router = createBrowserRouter([
   {
