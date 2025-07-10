52dcdab7aff004df09350ed788520ff710769fd3
anne durand <annedurand0709@gmail.com>
2025-07-10 14:43:48 +0200

    create page add artwork

---
+++ client/src/pages/AddArtworkPage/AddArtworkPage.tsx
@@ -0,0 +1,36 @@
+import "./AddArtworkPage.css";
+
+function AddArtworkPage() {
+  return (
+    <main className="add_artwork_page">
+      <section>
+        <h1>Ajouter une oeuvre</h1>
+        <form className="form">
+          <label htmlFor="titre"> Titre de l'oeuvre </label>
+          <input type="text" />
+
+          <label htmlFor=""> Description de l'oeuvre</label>
+          <input id="description" type="text" />
+
+          <label htmlFor=""> Catégorie de l'oeuvre</label>
+
+          <select id="categorie" name="categorie">
+            <option value="...">...</option>
+            <option value="photographie">Photographie</option>
+            <option value="dessin">Dessin</option>
+            <option value="peinture">Peinture</option>
+          </select>
+
+          <label htmlFor="titre"> Tags:(pour référencer l'oeuvre) </label>
+          <input type="text" />
+
+          <label htmlFor=""> Tarifs:</label>
+          <input type="text" />
+          <button type="submit">Ajouter</button>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default AddArtworkPage;
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -9,6 +9,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import NotFound from "./pages/Notfound/Notfound";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import AddArtworkPage from "./pages/AddArtworkPage/AddArtworkPage";
 
 const router = createBrowserRouter([
   {
@@ -46,6 +47,10 @@ const router = createBrowserRouter([
       {
         element: <ArtworkModificationPage />,
         path: "artwork-modification",
+      },
+      {
+        element: <AddArtworkPage />,
+        path: "add-artwork",
       },
     ],
   },
