6d68bcf5eeb2af3e21037755e9c79c7f9513a7b1
mohamed <aouis.mohamed@yahoo.fr>
2025-07-11 14:25:39 +0200

    work in progresss

--- client/src/pages/Favoritespage/Favoritespage.tsx
+++ client/src/pages/Favoritespage/Favoritespage.tsx
@@ -2,9 +2,8 @@ import "./Favoritespage.css";
 
 function Favoritespage() {
   return (
-    <main className="gallery_page">
+    <main className="favorites_page">
       <h1 className="title">Favoris</h1>
-      
     </main>
   );
 }
