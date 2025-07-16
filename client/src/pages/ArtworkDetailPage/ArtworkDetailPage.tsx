3fd0183fe73f2b57602d89776dac5e67ba2d7a9a
TRISTAN ZUBIARRAIN <TRISTANZUBIARRAIN15@gmail.com>
2025-07-16 12:33:58 +0200

    fetch front ArtworkDetailPage

--- client/src/pages/ArtworkDetailPage/ArtworkDetailPage.tsx
+++ client/src/pages/ArtworkDetailPage/ArtworkDetailPage.tsx
@@ -3,13 +3,18 @@ import { useParams } from "react-router";
 
 function ArtworkDetailPage() {
   const { id } = useParams();
+
   const [artwork, setArtwork] = useState<Artwork | null>(null);
 
   useEffect(() => {
     fetch(`http://localhost:3310/api/artwork/${id}`)
       .then((res) => res.json())
-      .then((data) => setArtwork(data))
-      .catch((error) => console.error("Erreur:", error));
+      .then((data) => {
+        setArtwork(data[0]);
+      })
+      .catch((error) => {
+        console.error("Erreur pendant fetch :", error);
+      });
   }, [id]);
 
   if (!artwork) {
@@ -21,6 +26,7 @@ function ArtworkDetailPage() {
       <h1>{artwork.title}</h1>
       <img src={artwork.image} alt={artwork.title} />
       <p>Prix: {artwork.price}€</p>
+      <p>Description: {artwork.description}</p>
       <p>Artiste: {artwork.artist_name}</p>
     </main>
   );
