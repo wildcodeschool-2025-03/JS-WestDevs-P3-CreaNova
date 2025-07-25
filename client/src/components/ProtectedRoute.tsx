cb4bf024c2ed28e766de53561426feff1055959a
ahmed firoum <ahmedfiroumk@gmail.com>
2025-07-25 12:35:54 +0200

    Added loading inside authcontext, had async problem. Had to change the json response refreshtoken

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -3,8 +3,10 @@ import { Navigate } from "react-router";
 import { useAuth } from "../hooks/useAuth";
 
 function ProtectedRoute({ children }: { children: ReactNode }) {
-  const { user } = useAuth();
-
+  const { user, isLoading } = useAuth();
+  if (isLoading) {
+    return <h1>Chargement...</h1>;
+  }
   if (!user || !user.isAdmin) {
     return <Navigate to="/" replace />;
   }
--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -6,6 +6,7 @@ export const AuthContext = createContext<null | Auth>(null);
 export const AuthProvider = ({ children }: Children) => {
   const [isLogged, setIsLogged] = useState(false);
   const [user, setUser] = useState<UserPayload | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch("http://localhost:3310/api/refresh-token", {
       credentials: "include",
@@ -20,10 +21,14 @@ export const AuthProvider = ({ children }: Children) => {
         if (data) {
           setUser(data);
         }
-      });
+        setIsLoading(false);
+      })
+      .catch(() => setIsLoading(false));
   }, []);
   return (
-    <AuthContext value={{ isLogged, setIsLogged, user, setUser }}>
+    <AuthContext
+      value={{ isLogged, setIsLogged, user, setUser, isLoading, setIsLoading }}
+    >
       {children}
     </AuthContext>
   );
