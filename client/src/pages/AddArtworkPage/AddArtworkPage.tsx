db58ce42e26905f158509287b786096825a8c558
ahmed firoum <ahmedfiroumk@gmail.com>
2025-07-30 12:22:39 +0200

    feat - Enhance artwork submission with file upload and tag filtering

--- client/src/pages/AddArtworkPage/AddArtworkPage.tsx
+++ client/src/pages/AddArtworkPage/AddArtworkPage.tsx
@@ -1,35 +1,48 @@
 import { Link, useParams } from "react-router";
 import "./AddArtworkPage.css";
+import { useState } from "react";
+import type { ChangeEvent, FormEvent } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { useAuth } from "../../hooks/useAuth";
 
 function AddArtworkPage() {
   const { userId } = useParams();
   const { isLogged } = useAuth();
+  const [file, setFile] = useState<File | undefined>();
 
-  const handleSubmit = (data: FormData) => {
-    const values = Object.fromEntries(data);
+  const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      if (selectedFile.size > 500 * 1024) {
+        toast.error("La taille de l'image ne doit pas être supérieur à 500ko");
+        e.target.value = "";
+        setFile(undefined);
+        return;
+      }
+      setFile(selectedFile);
+    }
+  };
 
-    const tags = [values.tag1, values.tag2, values.tag3]
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    data.append("user_account_id", String(userId));
+    const tags = [data.get("tag1"), data.get("tag2"), data.get("tag3")]
       .filter((tag) => typeof tag === "string" && tag.trim() !== "")
-      .map((tag) => (typeof tag === "string" ? tag.trim() : ""));
+      .join(",");
+    data.append("tags", tags);
 
-    const formData = {
-      ...values,
-      user_account_id: Number(userId),
-      price: Number(values.price),
-      tags,
-      mainCategory: values.mainCategory,
-    };
     fetch("http://localhost:3310/api/artworks", {
       method: "POST",
       credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
+      body: data,
+    }).then((res) => {
+      if (res.ok) {
+        toast.success("Oeuvre ajoutée avec succès !");
+      } else {
+        throw new Error("Erreur lors de l'ajout");
+      }
     });
-    toast.success("Oeuvre ajoutée avec succès !");
   };
   if (!isLogged) {
     return (
@@ -49,15 +62,30 @@ function AddArtworkPage() {
       <main className="add-artwork-page">
         <section>
           <h1>Ajouter une oeuvre</h1>
-          <form className="form" action={handleSubmit}>
+          <form className="form" onSubmit={handleSubmit}>
             <label htmlFor="title"> Titre de l'oeuvre </label>
             <input type="text" name="title" />
 
             <label htmlFor="description"> Description de l'oeuvre</label>
             <input id="description" type="text" name="description" />
 
-            <label htmlFor="image"> Image URL </label>
-            <input type="text" name="image" />
+            <input
+              type="file"
+              name="image"
+              id="artwork-image"
+              accept="image/png, image/jpg, image/jpeg"
+              onChange={handleFile}
+            />
+            <label htmlFor="artwork-image" className="file-label">
+              Choisir une image
+            </label>
+
+            {file && (
+              <section>
+                <p>Nom : {file.name}</p>
+                <p>Taille : {file.size} bytes</p>
+              </section>
+            )}
 
             <label htmlFor="price"> Tarif:</label>
             <input type="text" name="price" />
