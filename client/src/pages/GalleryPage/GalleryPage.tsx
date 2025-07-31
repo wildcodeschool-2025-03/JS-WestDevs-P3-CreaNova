bc5ff17b87dce98599e0e1e2dbea0a00b329831d
ahmed firoum <ahmedfiroumk@gmail.com>
2025-07-31 11:32:45 +0200

    Added favorite svg

--- client/src/pages/GalleryPage/GalleryPage.tsx
+++ client/src/pages/GalleryPage/GalleryPage.tsx
@@ -72,7 +72,20 @@ function GalleryPage() {
               onClick={() => handleAddToFavorites(artwork.id)}
               className="favorite"
             >
-              <img src="/img/favorite.png" alt="favorite" />
+              <label className="container">
+                <input type="checkbox" onClick={(e) => e.stopPropagation()} />
+                <svg
+                  id="Layer_1"
+                  version="1.0"
+                  viewBox="0 0 24 24"
+                  xmlSpace="preserve"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                  <title>Favori</title>
+                  <path d="M16.4,4C14.6,4,13,4.9,12,6.3C11,4.9,9.4,4,7.6,4C4.5,4,2,6.5,2,9.6C2,14,12,22,12,22s10-8,10-12.4C22,6.5,19.5,4,16.4,4z" />
+                </svg>
+              </label>
             </button>
 
             <Link to={`/artwork/${artwork.id}`}>
