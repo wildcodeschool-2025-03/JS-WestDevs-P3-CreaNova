c47d53b6d9c717009a6476d442d8d720072015fc
ahmed firoum <ahmedfiroumk@gmail.com>
2025-08-06 12:35:31 +0200

    Created login test

--- client/tests/login.spec.ts
+++ client/tests/login.spec.ts
@@ -1,7 +1,24 @@
 import { expect, test } from "@playwright/test";
+import "dotenv/config";
 
 test("A user should be logged in", async ({ page }) => {
-  await page.goto("https://localhost:3000/login");
+  const password = process.env.PASSWORD_TEST;
 
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.goto("http://localhost:3000/login");
+
+  const emailInput = page.locator("input[type='email']");
+
+  await emailInput.fill("abolfazl@example.com");
+
+  const passwordInput = page.locator("input[type='password']");
+
+  await passwordInput.fill(password as string);
+
+  const button = page.locator("button");
+
+  await button.click();
+
+  const toast = page.getByText("Vous êtes connecté", { exact: true });
+
+  await expect(toast).toBeVisible();
 });
